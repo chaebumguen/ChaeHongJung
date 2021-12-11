a980e077a705f07bab00ad768cd424d492bd446c
honggilgim <48556879+honggilgim@users.noreply.github.com>
2021-12-11 11:10:33 +0900

    auth 기능 구현 완료

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 
@@ -28,7 +29,7 @@ mongoose.connect('mongodb+srv://ghdrlf:1234@chae.lnozb.mongodb.net/myFirstDataba
 app.get('/', (req,res) => res.send('Hello World'))
 
 
-app.post('/register', (req,res) => {
+app.post('/api/user/register', (req,res) => {
     //회원 가입시 필요한 정보들을 클라이언트에서 가져오면, 그것들을 데이터 베이스에 넣어주는 코드
 
     const user = new User(req.body)
@@ -43,7 +44,7 @@ app.post('/register', (req,res) => {
 })
 
 
-app.post('/login', (req,res) => {
+app.post('/api/user/login', (req,res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email:req.body.email }, (err,user) => {
         if(!user) {
@@ -62,8 +63,7 @@ app.post('/login', (req,res) => {
         //3.비밀번호도 같다면 token 생성
         user.generateToken((err,user)=>{
           if(err) return res.status(400).send(err);
-          //token을 어디에 저장을 해서 보관해야함 쿠키나 로컬 저장소나
-          //일단 우린 쿠키에 저장한다
+          //token을 쿠키에 저장
           res.cookie("x_auth",user.token)
           .status(200)
           .json({loginSuccess:true,userId:user._id})
@@ -71,6 +71,16 @@ app.post('/login', (req,res) => {
 
 })
 })
+})
+//auth : 권한에 맞는 페이지에만 접근하게 해줌
+app.get('/api/user/auth', auth, (req,res) => {
+
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role ===0 ? false : true,
+        
+    })
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -75,6 +75,20 @@ userSchema.methods.generateToken= function(cb){
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    
+    //토큰을 디코드 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 토큰과 디비에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
+}
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
